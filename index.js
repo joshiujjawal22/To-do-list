e096f76c8bdd541f4c73a235a68248b0a1c11b06
joshiujjawal22 <joshiujjawal22@gmail.com>
2020-05-06 21:54:40 +0530

    Updating the state

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-function createStore() {
+// Library Code
 
+function createStore(reducer) {
 
+	// The store should have four parts
+  // 1. The state
+  // 2. Get the state.
+  // 3. Listen to changes on the state.
+  // 4. Update the state
+  
 	let state
 	let listeners = []
 
@@ -13,9 +20,27 @@ function createStore() {
 		}
 	}
 
-	
+	const dispatch = (action) => {
+		state = reducer(state, action)
+		listeners.forEach((listener) => listener())
+	}
+
+
 	return {
 		getState,
-		subscribe
+		subscribe,
+		dispatch,
 	}
 }
+
+
+// App Code
+function todos (state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo])
+  }
+
+  return state
+}
+
+
