67a9e4bcdf22b23fcbf5746262b88953827f2fb9
joshiujjawal22 <joshiujjawal22@gmail.com>
2020-05-15 00:25:26 +0530

    Added goals and todos in components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
+import ConnectedTodos from './Todos'
+import ConnectedGoals from './Goals'
+import { connect } from 'react-redux'
+import {
+  handleInitialData
+} from '../actions/shared'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello World
-      </div>
-    )
+class App extends React.Component {
+  componentDidMount () {
+    const { dispatch } = this.props
+
+    dispatch(handleInitialData())
   }
-}
 
-export default App;
+		render() {
+			if (this.props.loading === true) {
+		      return <h3>Loading</h3>
+		    }
+		    return (
+		    	<div>
+		    		<ConnectedTodos />
+        			<ConnectedGoals />
+        		</div>
+        		)
+        	}
+
+        }
+
+export default connect((state) => ({
+  loading: state.loading
+}))(App) 
